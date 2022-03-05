c8ce1d32475305cebcbba4650bac4fb3f3e4c10b
David G. White <dave-white@users.noreply.github.com>
2022-03-05 18:41:49 -0500

    Helper functions aren't really needed here, so threw everything into main(). Resulting simplification obviates gitall.h.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ endif
 
 .PHONY: install
 
-gitall.o: %.o: %.c %.h config.h
+gitall.o: %.o: %.c config.h
 	gcc -o $@ $<
 
 install: gitall.o
--- gitall.c
+++ gitall.c
@@ -1,105 +1,27 @@
-#include "gitall.h"
+#include <stdlib.h>
+#include <glob.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fts.h>
+#include <fnmatch.h>
+#include "config.h"
 
-static char *run(char **argv)
-{
-  int link[2];
-  pid_t pid;
-  char *out = (char *)calloc(4096, sizeof(char));
+#define die(e) do { fprintf(stderr, "%s\n", e); exit(EXIT_FAILURE); } while (0);
 
-  if (pipe(link)==-1) die("pipe");
+static glob_t glob_rslt;
+const int glob_flg = GLOB_APPEND;
 
-  if ((pid = fork()) == -1) die("fork");
+struct strary {
+    int strc;
+    char **strv;
+} strary;
 
-  if(pid == 0) {
-    dup2 (link[1], STDOUT_FILENO);
-    close(link[0]);
-    close(link[1]);
-    execvp(argv[0], argv);
-    die("execvp");
-  } else {
-    close(link[1]);
-    int nbytes = read(link[0], out, 4095*sizeof(char));
-    wait(NULL);
-  }
-  return out;
-}
-
-static int run_git(char *repo, int gitargc, char *gitargv[])
-{
-  char **argv = (char **)calloc(4+gitargc, sizeof(char *));
-  argv[0] = "git";
-  argv[1] = "-C";
-  argv[2] = repo;
-  for (int argi=0; argi<gitargc; argi++) argv[3+argi] = gitargv[1+argi];
-  argv[4 - 1 + gitargc] = NULL;
-
-  char *out = run(argv);
-
-  if (strlen(out) > 0) {
-    printf("\n==== %s ", repo);
-    int i = 0;
-    for (int i=0; i < 79 - (strlen(repo) + 6); i++) printf("=");
-    printf("\n");
-    printf("%s\n", out);
-  }
-
-  free(out);
-  free(argv);
-  return 0;
-}
-
-static int rd_gitignore(struct strary *igns)
-{
-  igns->strc = 0;
-  igns->strv = NULL;
-
-  FILE *ignf = fopen(GITIGNOREF, "r");
-  if (ignf == NULL) return 1;
-  char *ln;
-  size_t n;
-  ssize_t len;
-  igns->strv = (char **)calloc(127, sizeof(char *));
-  int i = 0;
-  while ((len = getline(&ln, &n, ignf)) != -1) {
-    char *ptr = (char *)calloc(127, sizeof(char));
-    strncpy(ptr, ln, len-1);
-    igns->strv[i] = ptr;
-    i++;
-    //ign_lst = realloc(ign_lst, (i+1)*sizeof(char *));
-    /* memset(ign_lst[i-1], 0, 80*sizeof(char)); */
-    /* strcpy(ign_lst[i-1], "test"); */
-    /* ign_lst[i-1] = "test"; */
-  }
-  igns->strc = i;
-  fclose(ignf);
-  return 0;
-}
-
-static int dglob(char *root)
-{
-  char * const root_lst[] = {root, NULL};
-  const int flg = FTS_NOSTAT|FTS_PHYSICAL|FTS_SEEDOT;
-  FTS *tree = fts_open(root_lst, flg, NULL);
-  FTSENT *fent;
-  char *pat = (char *)calloc(511, sizeof(char));
-  while ((fent = fts_read(tree))) {
-    if (fent->fts_info == FTS_D) {
-      //pat = realloc(pat, fent->fts_pathlen*sizeof(char)+40);
-      //memset(pat, 0, fent->fts_pathlen*sizeof(char)+40);
-      memset(pat, 0, 511*sizeof(char));
-      strncpy(pat, fent->fts_path, fent->fts_pathlen);
-      strcat(pat, "/.git");
-      glob(pat, glob_flg, NULL, &glob_rslt);
-    } else {
-      continue;
-    }
-  }
-  fts_close(tree);
-  free(pat);
-  return 0;
-}
-
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
   if (argc < 2) {
     printf("No git operation given.\n");
@@ -108,34 +30,86 @@ int main(int argc, char *argv[])
 
   char *home = getenv("HOME");
   printf("Globbing for local git repos in %s.\n", home);
-  dglob(home);
-  if (&glob_rslt == NULL || glob_rslt.gl_pathc == 0) {
+  char * const root_lst[] = {home, NULL};
+  const int fts_flg = FTS_NOSTAT|FTS_PHYSICAL|FTS_SEEDOT;
+  FTS *ftree = fts_open(root_lst, fts_flg, NULL);
+  FTSENT *fnode;
+  int pat_szmult = 1;
+  char *pat = (char *)calloc(128, sizeof(char));
+  while ((fnode = fts_read(ftree))) {
+    if (fnode->fts_info == FTS_D) {
+      if (fnode->fts_pathlen+5 > 128*pat_szmult - 8) {
+	pat_szmult++;
+	pat = realloc(pat, 128*pat_szmult*sizeof(char));
+      }
+      memset(pat, 0, fnode->fts_pathlen+5+8);
+      strncpy(pat, fnode->fts_path, fnode->fts_pathlen);
+      strcat(pat, "/.git");
+      glob(pat, glob_flg, NULL, &glob_rslt);
+    } else {
+      continue;
+    }
+  }
+  fts_close(ftree);
+  free(pat);
+  if (glob_rslt.gl_pathc < 1) {
     printf("No repos globbed.\n");
     return 0;
   }
 
   printf("Running `git %s` on local repos.\n", argv[1]);
 
+  // Read the ignore file.
   struct strary igns;
-  if (strcmp(argv[1], "pull") == 0) {
-      igns.strc = 0;
-      igns.strv = NULL;
-  } else {
-      rd_gitignore(&igns);
-      if (igns.strc > 0) {
-	  printf("Ignoring: %s", igns.strv[0]);
-	  for (int i=1; i<igns.strc; i++) printf(", %s", igns.strv[i]);
-	  printf("\n");
+  igns.strc = 0;
+  // igns.strv = NULL;
+  if (strcmp(argv[1], "pull") != 0) {
+    FILE *ignf = fopen(GITIGNOREF, "r");
+    if (ignf != NULL) {
+      igns.strv = (char **)calloc(128, sizeof(char *));
+      int ignv_szmult = 1;
+      char *line = (char *)calloc(128, sizeof(char));
+      size_t n;
+      ssize_t linelen;
+      char *ptr;
+      while ((linelen = getline(&line, &n, ignf)) != -1) {
+	ptr = (char *)calloc(linelen, sizeof(char));
+	strncpy(ptr, line, linelen-1);
+	igns.strv[igns.strc] = ptr;
+	igns.strc++;
+	if (igns.strc > 128*ignv_szmult - 8) {
+	  ignv_szmult++;
+	  igns.strv = realloc(igns.strv, 128*ignv_szmult*sizeof(char *));
+	}
       }
+      fclose(ignf);
+    }
+    if (igns.strc > 0) {
+      printf("Ignoring: %s", igns.strv[0]);
+      for (int i=1; i<igns.strc; i++) printf(", %s", igns.strv[i]);
+      printf("\n");
+    }
   }
 
-  char *repo = (char *)calloc(80, sizeof(char));
+  char **gitargv = (char **)calloc(4+(argc-1), sizeof(char *));
+  gitargv[0] = "git";
+  gitargv[1] = "-C";
+  for (int i=0; i<(argc-1); i++)
+    gitargv[3+i] = argv[1+i];
+  char *repo = (char *)calloc(128, sizeof(char));
+  int repo_szmult = 1;
   int len = 0;
   int skip;
+  FILE *output;
+  int tuyau[2];
+  pid_t pid;
   for (int i=0; i<glob_rslt.gl_pathc; i++) {
-    memset(repo, 0, 80*sizeof(char));
     len = strlen(glob_rslt.gl_pathv[i]) - 5;
-    //repo = realloc(repo, sizeof(char)*(len+1));
+    if (len > 128*repo_szmult - 8) {
+      repo_szmult++;
+      repo = realloc(repo, 128*repo_szmult*sizeof(char));
+    }
+    memset(repo, 0, (len+1)*sizeof(char));
     strncpy(repo, glob_rslt.gl_pathv[i], len);
     skip = 0;
     for (int j=0; j<igns.strc; j++) {
@@ -144,11 +118,47 @@ int main(int argc, char *argv[])
 	break;
       }
     }
-    if (skip) continue;
-    run_git(repo, argc - 1, argv);
+    if (skip)
+      continue;
+
+    gitargv[2] = repo;
+
+    if(pipe(tuyau))
+      die("tuyau failed.\n");
+
+    pid = fork();
+    if (pid < (pid_t) 0) {
+      die("Fork failed.\n");
+    } else if (pid == 0) {
+      // Child
+      close(tuyau[0]); // Close readable end.
+      dup2 (tuyau[1], STDOUT_FILENO);
+      // dup2 (tuyau[1], STDERR_FILENO);
+      close(tuyau[1]);
+      execvp(gitargv[0], gitargv);
+      die("Spawned execvp going down.\n");
+    } else {
+      // Parent
+      close(tuyau[1]); // Close writable end.
+      output = fdopen(tuyau[0], "r");
+      int c;
+      if ((c = fgetc(output)) != EOF) {
+        printf("\n==== %s ", repo);
+        for (int i=0; i < 79 - (strlen(repo) + 6); i++) printf("=");
+        printf("\n");
+        putchar(c);
+        while ((c = fgetc(output)) != EOF)
+      putchar(c);
+      }
+      fclose(output);
+      wait(NULL);
+    }
   }
+
+  free(gitargv);
   free(repo);
-  for (int i=0; i<igns.strc; i++) free(igns.strv[i]);
+  for (int i=0; i<igns.strc; i++)
+    free(igns.strv[i]);
   globfree(&glob_rslt);
   return 0;
 }
